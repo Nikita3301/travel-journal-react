82fcf2d0885f7c33965c0312856c7a396f9962ad
Nikita Riabov <nikita.riabov.itis.2019@lpnu.ua>
2023-01-24 20:10:58 +0200

    commit 1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,6 +16,18 @@ export default function Home(){
     })
 
 
+    const [user, setUser] = useState([]);
+
+    const fetchData = () => {
+        return fetch("https://api.opentripmap.com/0.1/en/places/bbox?lon_min=23.635978&lat_min=49.770285&lon_max=24.285872&lat_max=49.985313&format=geojson&apikey=5ae2e3f221c38a28845f05b6f5a6dc7747c993a6f20028b8b8e69b9a")
+            .then((response) => response.json())
+            .then((data) => setUser(data));
+    }
+
+    useEffect(() => {
+        fetchData();
+    },[])
+
     return (
         <div className="home-page">
             <div className="background-image-container">
@@ -26,6 +38,9 @@ export default function Home(){
                 <div className="destinations-data-container">
                     {data}
                 </div>
+                    {user && user.length > 0 && user.map(userObj => (
+                        <p>{userObj.type}</p>
+                    ))}
             </div>
         </div>
     )
